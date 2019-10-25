10cf242202856798e59b7289bd3cfce94d157472
Mengxiao <mengxiao.zhang@wsu.edu>
2019-10-24 23:56:43 -0700

    Start project for Data Science

---
+++ Cpt_s575/project/test.r
@@ -0,0 +1,3 @@
+## data <- read.csv('https://scads.eecs.wsu.edu/wp-content/uploads/2017/10/AM_strains_clean.csv', na.string = '?')
+data <- read.csv('~/Documents/wsu/Cpt_s575/project/data/AM_strains_clean.csv', na.string='?', fileEncoding='utf-16', sep='\t')
+
