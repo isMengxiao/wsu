327c4f74d316aaa619102b2c3892488034dabfd4
Mengxiao.Zhang <350535629@qq.com>
2019-10-26 19:11:36 -0700

    update hw5 for datascience

--- Cpt_s575/project/test.r
+++ Cpt_s575/project/test.r
@@ -1,3 +1,7 @@
+library(dplyr)
+library(tidyr)
+
 ## data <- read.csv('https://scads.eecs.wsu.edu/wp-content/uploads/2017/10/AM_strains_clean.csv', na.string = '?')
 data <- read.csv('~/Documents/wsu/Cpt_s575/project/data/AM_strains_clean.csv', na.string='?', fileEncoding='utf-16', sep='\t')
 
+head(data)
